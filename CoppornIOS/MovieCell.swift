0e81dd7a823635565f851a219f180e24b63397df
vader1359 <vader1359@gmail.com>
2017-06-19 07:07:00 +0700

    Load cell one side

---
+++ CoppornIOS/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  CoppornIOS
+//
+//  Created by Vader1359 on 6/19/17.
+//  Copyright © 2017 vader1359. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    // List of outlets
+    @IBOutlet weak var posterImg: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CoppornIOS/MovieViewController.swift
+++ CoppornIOS/MovieViewController.swift
@@ -8,28 +8,54 @@
 
 import UIKit
 
-class MovieViewController: UIViewController {
-
+class MovieViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    // List of outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-    }
+        
+        tableView.backgroundColor = .clear
+        tableView.separatorColor = .clear
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell") as! MovieCell
+        
+        cell.backgroundColor = .clear
+        cell.titleLabel.text = "Wonder Woman"
+        cell.overviewLabel.text = "An Amazon princess comes to the world of Man to become the greatest of the female super heroes."
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
